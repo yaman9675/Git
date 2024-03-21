bf2b0e434ba9080ad19bf4fdd136154f74b2c47e
Yaman Saini <ys726507@gmail.com>
2024-03-21 22:53:27 +0530

    commit 1

--- hungry.py
+++ hungry.py
@@ -1,8 +1,13 @@
-hungry = input('Are you hungry')
+hungry = input('Are you hungry?')
 if hungry=="yes":
     print('eat samosa')
     print('eat pizza')
     print('eat burger')
     print('eat fries')
 else:
-    print('Go Play')
+    thristy = input("are you thristy?")
+    if thristy=="yes":
+        print("drink water")
+        print('drink juice')
+    else:
+        print('go play')
